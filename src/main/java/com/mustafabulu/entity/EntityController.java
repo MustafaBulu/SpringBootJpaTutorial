1e278dcb314c0eeecd4d699355170b6521224991
mustafabulu <you@example.com>
2022-01-30 01:40:55 +0300

    Insert, Find, Update, Delete operations

---
+++ src/main/java/com/mustafabulu/entity/EntityController.java
@@ -0,0 +1,115 @@
+package com.mustafabulu.entity;
+
+
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@Log4j2
+public class EntityController {
+
+
+
+
+    @Autowired(required = true)
+    private IStudentRepository iStudentRepository;
+
+    //Create
+    //http://localhost:3333/jpa/student/insert
+
+    @GetMapping("/jpa/student/insert")
+    @ResponseBody // bunu yazmazsak return kısmındaki sayfayı döndürmeye çalışır
+    public String insertStudent(){
+        StudentEntity studentEntity= new StudentEntity(0,"Mustafa","Bulu","Computer Engineering");
+        iStudentRepository.save(studentEntity); // 0 ise insert yapıyor 0 dan farklı ise güncelleme yapıyor.
+        log.info(studentEntity.getStudentid() + ""+ studentEntity.getStudentName(),studentEntity.getStudentSurName(),studentEntity.getStudentDepartment());
+        return studentEntity.getStudentid() + "  " + studentEntity.getStudentName()+ " " +studentEntity.getStudentSurName() + "  "+studentEntity.getStudentDepartment();
+    }
+
+    //Find
+    //http://localhost:3333/jpa/student/find
+
+    @GetMapping("/jpa/student/find/{id}")
+    @ResponseBody // bunu yazmazsak return kısmındaki sayfayı döndürmeye çalışır
+    public String findStudent(@PathVariable(name = "id") Long studentid){
+        //name kısmındaki id ile yukarıdaki kısım aynı olmak zorunda
+        java.util.Optional<StudentEntity> optional = iStudentRepository.findById(studentid);
+        if (optional.isPresent()){
+            StudentEntity studentEntity=optional.get();
+            log.info(studentEntity.toString());
+            return studentEntity.toString();
+
+        }else{
+
+            return "Öğrenci Numarası:  " + studentid+ "   olan öğrenci bulunamadı";
+        }
+
+
+    }
+
+    //Update
+    //http://localhost:3333/jpa/student/update/2?studentName=guncelisim&studentSurname=guncelsoyadi&studentDepartment=gunceldepartman
+
+    @GetMapping("/jpa/student/update/{id}")
+    @ResponseBody
+    public String updateStudent(
+            @PathVariable(name = "id") Long studentid,
+            @RequestParam(name = "studentName") String studentName,
+            @RequestParam(name = "studentSurname") String studentSurname,
+            @RequestParam(name = "studentDepartment") String studentDepartment
+
+    ){
+    java.util.Optional<StudentEntity> optional=iStudentRepository.findById(studentid);
+
+
+    if (optional.isPresent()){
+        StudentEntity studentEntity=optional.get();
+        studentEntity.setStudentName(studentName);
+        studentEntity.setStudentSurName(studentSurname);
+        studentEntity.setStudentDepartment(studentDepartment);
+        iStudentRepository.save(studentEntity); // save işlemini burada yapıyoruz.
+        return "Guncelleme Basarili  " + studentEntity;
+    }else{
+        return "Guncelleme Basarisiz"+ studentid;
+
+    }
+
+
+    }
+
+
+    //Delete
+    //http://localhost:3333/jpa/student/delete/2
+
+    @GetMapping("/jpa/student/delete/{id}")
+    @ResponseBody
+    public String deleteStudent(@PathVariable(name = "id") Long studentid){
+
+        java.util.Optional<StudentEntity> optional=iStudentRepository.findById(studentid);
+
+
+        if (optional.isPresent()){
+            StudentEntity studentEntity= optional.get();
+            iStudentRepository.delete(studentEntity);
+
+            return "Silme Basarili  " + studentEntity.toString();
+
+
+        }else{
+            return "Silme Basarisiz";
+
+
+        }
+
+    }
+
+
+
+
+}
